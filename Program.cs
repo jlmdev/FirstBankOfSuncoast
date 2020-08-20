00494ed2e5eb735058a71f41a7fdf787241055fc
Josh Mann <jlmann72@gmail.com>
2020-08-20 15:56:21 -0400

    Able to isolate and display Checking transactions

--- Program.cs
+++ Program.cs
@@ -1,12 +1,100 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace FirstBankOfSuncoast
 {
-  class Program
-  {
-    static void Main(string[] args)
+    // Create Transaction Class
+    class Transaction
     {
-      Console.WriteLine("Welcome to C#");
+        // String: Description
+        public string Description { get; set; }
+        // Int: Amount
+        public double Amount { get; set; }
+        // Type: String (Deposit or Withdrawal)
+        public string Type { get; set; }
+        // Account: String (Checking or Savings)
+        public string Account { get; set; }
+        // 
+        public string LineItem()
+        {
+            var lineItem = $"{Description} {Type} {Account} {Amount}";
+            return lineItem;
+        }
     }
-  }
+    class Program
+    {
+        // Checking Transaction List
+        public static IEnumerable<Transaction> GetCheckingTransactions(List<Transaction> transactions)
+        {
+            var checkingTransactions = transactions.Where(transaction => transaction.Account == "Checking");
+            return checkingTransactions;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the First Bank of Suncoast");
+            // TODO: Test Transactions
+
+            var transactions = new List<Transaction>()
+            {
+              new Transaction
+              {
+                Description = "Paycheck",
+                Amount = 2538.67,
+                Type = "Deposit",
+                Account = "Checking",
+              },
+              new Transaction
+              {
+                Description = "Savings Deposit",
+                Amount = 500.00,
+                Type = "Deposit",
+                Account = "Savings",
+              },
+              new Transaction
+              {
+                Description = "Cable Bill",
+                Amount = 175.35,
+                Type = "Withdrawal",
+                Account = "Checking",
+              },
+              new Transaction
+              {
+                Description = "IRA",
+                Amount = 250,
+                Type = "Withdrawal",
+                Account = "Savings",
+              },
+            };
+
+            // Show Transactions
+            // foreach (var transaction in transactions)
+            // {
+            //     Console.WriteLine(transaction.LineItem());
+            // }
+
+            // Create Checking View
+            Console.Clear();
+            Console.WriteLine("*** Checking ***\n");
+
+            var checkingAccount = new List<Transaction>();
+
+            foreach (var item in GetCheckingTransactions(transactions))
+            {
+                checkingAccount.Add(item);
+                Console.WriteLine(item.LineItem());
+            }
+
+
+
+
+
+
+            // PEDAC
+            // TODO: Load or Create File
+            // TODO: Create Checking View
+            // TODO: Create Savings View
+            // TODO: Create Transactions List
+        }
+    }
 }
